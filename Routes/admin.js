3a49bc1f3c9ac73651796793df6e71f5e3627240
Tamana543 <tamanafarzami33@gmail.com>
2025-02-21 09:43:40 +0430

    Shop File Updated

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,5 +1,5 @@
-const express = import("express")
-const router = express.Router()
+const express = require("express")
+const router = express.Router();
 
 router.get("/app.product",(req,res,next)=> {
      res.send("<form action='/product' method='POST' ><input type = 'text' name ='title' /> <button type ='submit'>Add To Shop </button></form>")
@@ -9,4 +9,4 @@ router.get("/app.product",(req,res,next)=> {
 router.post("/product",(req,res,next)=> {
      res.redirect('/')
 })
-module.exports = "router"
+module.exports = router;
--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
 const adminRouter = require("./Routes/admin")
+const shapRouter = require("./Routes/shop")
+
 
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(adminRouter)
+app.use(shapRouter)
 app.listen(5430)
