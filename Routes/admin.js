f97efbbff149b5c71682f851815b3a2b8b65887f
Tamana543 <tamanafarzami33@gmail.com>
2025-02-23 08:04:37 +0430

    Path Updated

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,9 +1,9 @@
 const path = require("path")
 const express = require("express")
 const router = express.Router();
-
+const rootPath = require("../utill/path")
 router.get("/app.product",(req,res,next)=> {
- res.sendFile(path.join(__dirname,"../","views","addProduct.html"))
+ res.sendFile(path.join(rootPath,"views","addProduct.html"))
 })
 router.post("/product",(req,res,next)=> {
      res.redirect('/')
--- Routes/shop.js
+++ Routes/shop.js
@@ -1,7 +1,8 @@
 const path = require("path")
 const express = require("express")
 const router = express.Router()
+const rootPath = require("../utill/path")
 router.get("/",(req,res,next)=> {
-     res.sendFile(path.join(__dirname,"../","views","shop.html")) // To connect your HTML, path creates a path the join make the url, the __dirname go through all dirictoryies in your PC , ../ goes one level up .
+     res.sendFile(path.join(rootPath,"views","shop.html")) // To connect your HTML, path creates a path the join make the url, the __dirname go through all dirictoryies in your PC , ../ goes one level up .
 })
 module.exports = router
