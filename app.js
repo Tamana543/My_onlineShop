5f3e3417c1018f8461ae4ebb4a2a2297d562f7e3
Tamana543 <tamanafarzami33@gmail.com>
2025-02-24 08:59:45 +0430

    Style Added

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const bodyParser = require("body-parser")
 const app = express()
 const adminRouter = require("./Routes/admin")
 const shapRouter = require("./Routes/shop")
+const homeRouter = require("./Routes/home");
 const path = require("path")
 
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(adminRouter)
 app.use(shapRouter)
-
+app.use(homeRouter)
 app.use((req,res,next) => {
 res.status(404).sendFile(path.join(__dirname,"./","views","404.html"))
 })
