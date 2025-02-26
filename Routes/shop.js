512f865e902ddf519176683e972b57d6c21c87b6
Tamana543 <tamanafarzami33@gmail.com>
2025-02-26 09:23:26 +0430

    Pug File updated

--- Routes/shop.js
+++ Routes/shop.js
@@ -4,7 +4,8 @@ const router = express.Router()
 const rootPath = require("../utill/path")
 const adminData = require("./admin")
 router.get("/",(req,res,next)=> {
-     console.log(adminData.products);
-     res.sendFile(path.join(rootPath,"views","shop.html")) // To connect your HTML, path creates a path the join make the url, the __dirname go through all dirictoryies in your PC , ../ goes one level up .
+     // console.log(adminData.products);
+     // res.sendFile(path.join(rootPath,"views","shop.html")) // To connect your HTML, path creates a path the join make the url, the __dirname go through all dirictoryies in your PC , ../ goes one level up .
+     res.render("shop") // express for more information 
 })
 module.exports = router
