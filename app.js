a06d495b030984d78699d974f9b67c8b9384b267
Tamana543 <tamanafarzami33@gmail.com>
2025-02-28 08:08:19 +0430

    Foleder Updated: 404.pug file complete it

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.use(adminData.routs)
 app.use(shapRouter)
 app.use(homeRouter)
 app.use((req,res,next) => {
-res.status(404).sendFile(path.join(__dirname,"./","views","404.html"))
+res.status(404).render('404',{pageTitle: 'Page Not Found'})
 })
 app.listen(5430)
