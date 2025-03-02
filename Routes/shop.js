34f0b438117d7d613e2435c9493d1f9595b761d6
Tamana543 <tamanafarzami33@gmail.com>
2025-03-02 08:11:16 +0430

    Folder Updated:shop.css and card style fixed

--- Routes/shop.js
+++ Routes/shop.js
@@ -7,7 +7,7 @@ router.get("/",(req,res,next)=> {
      // console.log(adminData.products);
      // res.sendFile(path.join(rootPath,"views","shop.html")) // To connect your HTML, path creates a path the join make the url, the __dirname go through all dirictoryies in your PC , ../ goes one level up .
      const products = adminData.products;
-  console.log(products);
+//   console.log(products);
      res.render("shop",{prods : products, PageTitle : "shop"}) // express for more information 
 })
 module.exports = router
