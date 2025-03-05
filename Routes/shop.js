87b8bbb1e34e715e605260620124b779cd9f8ef8
Tamana543 <tamanafarzami33@gmail.com>
2025-03-05 07:40:26 +0430

    Folder Updated : shop.ejs updated, common layout embadded

--- Routes/shop.js
+++ Routes/shop.js
@@ -8,6 +8,6 @@ router.get("/",(req,res,next)=> {
      // res.sendFile(path.join(rootPath,"views","shop.html")) // To connect your HTML, path creates a path the join make the url, the __dirname go through all dirictoryies in your PC , ../ goes one level up .
      const products = adminData.products;
 //   console.log(products);
-     res.render("shop",{prods : products, PageTitle : "shop",path:"/shop",hasProducts:products.length > 0}) // express for more information 
+     res.render("shop",{prods : products, pageTitle : "shop",path:"/shop",hasProducts:products.length > 0}) // express for more information 
 })
 module.exports = router
