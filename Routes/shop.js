bc4a1d0ecb0af2c95d761d6f1d5dfee3da338bfb
Tamana543 <tamanafarzami33@gmail.com>
2025-03-08 07:38:46 +0430

    file updated

--- Routes/shop.js
+++ Routes/shop.js
@@ -3,6 +3,8 @@ const express = require("express")
 const router = express.Router()
 const rootPath = require("../utill/path")
 const adminData = require("./admin")
+
+// router.get("/",rootPath)
 router.get("/",(req,res,next)=> {
      // console.log(adminData.products);
      // res.sendFile(path.join(rootPath,"views","shop.html")) // To connect your HTML, path creates a path the join make the url, the __dirname go through all dirictoryies in your PC , ../ goes one level up .
