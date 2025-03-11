b9dd14525a8cb5a1cf13b24278c0026c5233d489
Tamana543 <tamanafarzami33@gmail.com>
2025-03-11 08:20:10 +0430

    Folder Updated

--- controller/products.js
+++ controller/products.js
@@ -1,16 +1,17 @@
 const products =[];
-exports.getAddProducts = (req,res,next)=> {
-   
-     (req,res,next)=> {
-          res.render("addProduct",{pageTitle: "Add Product",path: '/app.product',formsCSS: true,
-               productCSS: true,
-               activeAddProduct: true}) 
-         }
-}
 exports.postproducts = (req,res,next)=> {
      products.push({title : req.body.title})
      res.redirect('/')
 }
+exports.getAddProducts = (req,res,next)=> {
+res.render("app.product",{pageTitle: "Add Product",
+     path: '/app.product',
+     formsCSS: true,
+               productCSS: true,
+               activeAddProduct: true}) 
+         
+}
+
 exports.productsShop = (req,res,next)=> {
      // console.log(adminData.products);
      // res.sendFile(path.join(rootPath,"views","shop.html")) // To connect your HTML, path creates a path the join make the url, the __dirname go through all dirictoryies in your PC , ../ goes one level up .
