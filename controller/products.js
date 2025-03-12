fbca72ecbe4eecbf213d99d4b75399cc711d4340
Tamana543 <tamanafarzami33@gmail.com>
2025-03-12 07:52:06 +0430

    Folder Updated: module added to products controller

--- controller/products.js
+++ controller/products.js
@@ -1,6 +1,7 @@
-const products =[];
+const Products = require("../module/product.js")
 exports.postproducts = (req,res,next)=> {
-     products.push({title : req.body.title})
+  const product = new Products(req.body.title)
+  product.save()
      res.redirect('/')
 }
 exports.getAddProducts = (req,res,next)=> {
@@ -17,5 +18,6 @@ exports.productsShop = (req,res,next)=> {
      // res.sendFile(path.join(rootPath,"views","shop.html")) // To connect your HTML, path creates a path the join make the url, the __dirname go through all dirictoryies in your PC , ../ goes one level up .
      // const products = adminData.products;
 //   console.log(products);
+    const products = Products.fetchAll()
      res.render("shop",{prods : products, pageTitle : "shop",path:"/shop",hasProducts:products.length > 0}) // express for more information 
 }
