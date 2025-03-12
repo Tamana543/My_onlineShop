baf7721a597b2346692c8ca109ea5a02fe1464d7
Tamana543 <tamanafarzami33@gmail.com>
2025-03-12 09:27:21 +0430

    Refactoring Completed

--- module/product.js
+++ module/product.js
@@ -1,35 +1,35 @@
 const { json } = require("body-parser");
 const fs = require("fs");
 const path = require('path')
+const p = path.join(path.dirname(process.mainModule.filename), 'data','products.json')
+const getProductsFromFile =(cb) => {
+     fs.readFile(p,(err,fileContent)=>{
+          if(err){
+               // return [];
+               cb([]);
+          }else {
 
+               cb(JSON.parse(fileContent))
+          }
+          // return JSON.parse(fileContent)
+          // rememeber it is a synchrouus function and the fetchAll function returns undifined. solution : rgive the function an parameter and call the function just by the parameter .
+     })
+}
 module.exports =class Products {
      constructor(t) {
           this.title = t
      }
      save() {
-       const p = path.join(path.dirname(process.mainModule.filename), 'data','products.json')
-       fs.readFile(p,(err,fileContent)=>{
-          // console.log(fileContent);
-          let products = [];
-          if(!err) {
-               products = JSON.parse(fileContent)
-          }
-          products.push(this)
-          fs.writeFile(p, JSON.stringify(products),(err)=>{
-               console.log(err);
+       getProductsFromFile(product => {
+
+            product.push(this)
+            fs.writeFile(p, JSON.stringify(product),(err)=>{
+                 console.log(err);
+            })
           })
-       })
      }
      static fetchAll(cb) {
-          const p = path.join(path.dirname(process.mainModule.filename), 'data','products.json')
-          fs.readFile(p,(err,fileContent)=>{
-               if(err){
-                    // return [];
-                    cb([]);
-               }
-               // return JSON.parse(fileContent)
-               cb(JSON.parse(fileContent))
-               // rememeber it is a synchrouus function and the fetchAll function returns undifined. solution : rgive the function an parameter and call the function just by the parameter .
-          })
+        getProductsFromFile(cb)
+          
      }
 }
