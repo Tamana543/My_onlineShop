3c6aecf8a33d30f8c88eb31af6e1b4cbf41d08fb
Tamana543 <tamanafarzami33@gmail.com>
2025-12-07 20:26:38 +0430

    user with sequalizer created

--- module/user.js
+++ module/user.js
@@ -1,3 +1,16 @@
 const data = require('../data/products.json')
 const sequalizer  = require('sequelize')
 
+const User =  sequalizer.define('userProduct',{
+     id : {
+          type : sequalizer.INTEGER,
+          autoIncrement : true, 
+          allowNull : false,
+          primaryKey : true
+     }, 
+     name : sequalizer.STRING,
+     email : {
+          type : sequalizer.STRING,
+          allowNull : false
+     }
+})
