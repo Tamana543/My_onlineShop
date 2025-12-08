bc4125838a862a6fe01e8aee8e64f768714f7ec8
Tamana543 <tamanafarzami33@gmail.com>
2025-12-08 16:53:43 +0430

    file updated

--- util/database.js
+++ util/database.js
@@ -1,1 +1,12 @@
 
+const mongodb = require('mongodb')
+const mongodbClient = mongodb.mongodbClient;
+let _db; 
+const mangoCreateDB = (cb)=>{
+    try {
+     mongodbClient.connect('mongodb://localhost:27017/')
+    } catch (error) {
+     console.log(error);
+     throw error
+    } 
+}
