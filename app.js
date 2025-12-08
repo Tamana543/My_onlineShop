5713599cd9ad941c8a82d26bdee6b30ad41c3c8a
Tamana543 <tamanafarzami33@gmail.com>
2025-12-08 17:20:38 +0430

    file updated

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const adminRoute = require("./Routes/admin")
 const shapRouter = require("./Routes/shop")
 const homeRouter = require("./Routes/home");
 const path = require("path")
+const { Collection } = require("mongodb")
 // const expressHandlebar = require('express-handlebars'); un commit this if you like to use handlebar
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname,'public')))
@@ -17,6 +18,12 @@ app.set('view engine' , 'ejs') // to tell the express go and ramder the pug dina
 // app.set('view engine' , 'hbs') // to tell the express go and ramder the handlebar (search about it) dinamic data ( compile it )
 app.use(shapRouter)
 app.use(homeRouter)
+// database : Mongoo
+const MONGOD_URL  =  '';
+const store = new mongostoreSession({
+     uri : MONGOD_URL,
+     Collection : 'sessions'
+})
 app.use((req,res,next) => {
 res.status(404).render('404',{pageTitle: 'Page Not Found'})
 })
