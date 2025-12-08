60eb1e8c159ba6c05cb7c33317c5aa24b4f1905a
Tamana543 <tamanafarzami33@gmail.com>
2025-12-08 17:03:52 +0430

    file updated

--- util/database.js
+++ util/database.js
@@ -4,9 +4,30 @@ const mongodbClient = mongodb.mongodbClient;
 let _db; 
 const mangoCreateDB = (cb)=>{
     try {
-     mongodbClient.connect('mongodb://localhost:27017/')
+     mongodbClient.connect('mongodb://localhost:27017/').then(client=>{
+          console.log("Created");
+          _db = client.db() 
+          cb()
+
+     }).catch(err=>{
+          console.log(err);
+          throw err
+     })
     } catch (error) {
      console.log(error);
      throw error
     } 
 }
+const getDb = (test)=>{
+
+     // console.log(_db);
+     if(_db){
+
+          return _db; // returning the database function 
+     }
+     
+     throw "database not found "
+} ;
+
+exports.mangoCreateDb = mangoCreateDb
+exports.getDb = getDb
