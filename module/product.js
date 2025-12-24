dff6f69b786695f9b2012e909a68a5ff3a80d6e3
Tamana543 <tamanafarzami33@gmail.com>
2025-12-24 19:38:32 +0430

    schema for mongoose added to the product

--- module/product.js
+++ module/product.js
@@ -1,4 +1,30 @@
+const mongoose = require('mongoose')
+const Scheme = mongoose.Schema;
 
+const SchemaFirstProduct = new Scheme({
+     title : {
+          type: String,
+          require : true
+     },
+     price : {
+          type: Number,
+          require : true
+     },
+     description : {
+          type : String,
+          require : true
+     },
+     imageUrl : {
+          type : String,
+          require : true
+     },
+     productId : {
+          type : Scheme.Types.ObjectId,
+          ref : 'User',
+          require: true
+     }
+});
+module.exports = mongoose.model('Product',SchemaFirstProduct)
 
 
 /**
