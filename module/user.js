944b3e952a411a59ff3341b1963f68c00b72a6a6
Tamana543 <tamanafarzami33@gmail.com>
2025-12-26 16:23:55 +0430

    add to cart method in user added

--- module/user.js
+++ module/user.js
@@ -26,7 +26,26 @@ const userMainSchema = new Schema({
 
                }
           ]
-     }
+     },
+     
 })
-
+userMainSchema.methods.addToCart = function(product){
+     // get the index of the product if it already exist
+     const productIndex = this.cart.items.findIndex(cb=>{
+          return cb.productId.toString()== product._id.toString()
+     }) ;
+     let productNewQuantity = 1 
+     const userUpdatedCart = [...this.cart.items]
+     
+     // check if product already exist or not 
+     if(productIndex >= 0) {
+         productNewQuantity = this.cart.items[productIndex].quantity + 1;
+         userUpdatedCart[productIndex].quantity = productNewQuantity
+     }else {
+          userUpdatedCart.push({
+               productId : product._id,
+               quantity : productNewQuantity
+          })
+     }
+}
 module.exports = mongoose.model('User',userMainSchema)
