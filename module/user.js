66567541d1dd3b138a42d668b78ef4c115287e84
Tamana543 <tamanafarzami33@gmail.com>
2025-12-26 15:46:47 +0430

    User schema recreated

--- module/user.js
+++ module/user.js
@@ -1,79 +1,31 @@
-// const data = require('../data/products.json')
-// const sequalizer  = require('sequelize')
+// mongoose 
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
 
-const mongoose = require('mongoose');
-const { RelationshipType } = require('sequelize/lib/errors/database/foreign-key-constraint-error');
-const schema = mongoose.Schema;
-
-const UserSchema = new schema({
-     email: {
-          type : String,
-          require: true
-     }
-     ,
-     password : {
-          type : String,
+const userMainSchema = new Schema({
+     email : {
+          type: String,
           require : true
-     }, 
+     },
+     password : {
+          type: String,
+          require : true
+     },
      cart : {
-          items : [
-               {
+          items : {
+                    type : Array,
                     productId : {
-                         type : schema.Types.ObjectId,
-                         require: true,
-                         ref : 'Product'
+                         ref : 'Product',
+                         require : true,
+                         type : Schema.Types.ObjectId
                     },
                     quantity : {
                          type : Number,
                          require : true
                     }
-               }
-          ]
+
+          }
      }
 })
 
-UserSchema.methods.addToCart = function(product){
-const cartProductIndex =this.cart.items.findIndex(cp => {
-     return cp.productId.toString()== product._id.toString()
-})
-
-let newQuantity = 1 
-const updatedCartItem = [...this.cart.items] ;
-
-if(cartProductIndex >= 0) {
-     newQuantity = this.cart.items[cartProductIndex].quantity + 1 
-     updatedCartItem[cartProductIndex].quantity = newQuantity
-}else {
-     updatedCartItem.push (
-          {
-               productId : product._id,
-               quantity : newQuantity
-          }
-     )
-}
-const updatedCart ={
-     items :updatedCartItem
- } ;
- this.cart = updatedCart 
- return this.save()
-
-}
-UserSchema .methods.clearCart = function(){
-     this.cart = {items : []}
-     return this.save()
-}
-module.exports = mongoose.model('User',UserSchema)
-
-// const User =  sequalizer.define('userProduct',{
-//      id : {
-//           type : sequalizer.INTEGER,
-//           autoIncrement : true, 
-//           allowNull : false,
-//           primaryKey : true
-//      }, 
-//      name : sequalizer.STRING,
-//      email : {
-//           type : sequalizer.STRING,
-//           allowNull : false
-//      }
-// })
+module.exports = mongoose.model('User',userMainSchema)
