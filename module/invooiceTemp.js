0fef93c076fcb9d2d042f1fe5c08548d157b6941
Tamana543 <tamanafarzami33@gmail.com>
2026-01-03 20:59:19 +0430

    invoice templete generator added

---
+++ module/invooiceTemp.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
+
+function generateHeader(doc) {
+  doc
+    .image("lamborghini-car-logo.jpg", 50, 45, { width: 50 })
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("ACME Inc.", 110, 57)
+    .fontSize(10)
+    .text("ACME Inc.", 200, 50, { align: "right" })
+    .text("123 Main Street", 200, 65, { align: "right" })
+    .text("New York, NY, 10025", 200, 80, { align: "right" })
+    .moveDown();
+}
