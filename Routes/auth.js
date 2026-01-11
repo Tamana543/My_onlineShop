30e2266d28d57eea83bc20af732ea8c79f23c928
Tamana543 <tamanafarzami33@gmail.com>
2026-01-11 15:50:01 +0430

    authentication from controller updated

---
+++ Routes/auth.js
@@ -0,0 +1,7 @@
+const authController = require('../controller/auth')
+const express = require('express')
+const router = express.Router();
+const User = require('../module/user')
+
+
+router.get('/login')
