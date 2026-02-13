dd382221c3412eb85b957b114cdf3d5dc27a43c4
Tamana543 <tamanafarzami33@gmail.com>
2026-02-13 05:58:30 +0430

    hambarger animation fixed

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,23 @@
 // for navBar
-const backdrop = document.querySelector('.backdrop');
+// const backdrop = document.querySelector('.backdrop');
 const sideDrawer = document.querySelector('.mobile-nav');
-const menuToggle = document.querySelector('#side-menu-toggle');
-const hamburger = document.querySelector('.hamburger')
+const hamburger = document.querySelector('#hamburger-6');
 
-function backdropClickHandler() {
-  backdrop.style.display = 'none';
-  sideDrawer.classList.remove('open');
-}
+// function backdropClickHandler() {
+//   backdrop.style.display = 'none';
+//   sideDrawer.classList.remove('open');
+// }
 
-function menuToggleClickHandler() {
-  backdrop.style.display = 'block';
-  sideDrawer.classList.add('open');
-}
+// function menuToggleClickHandler() {
+//   backdrop.style.display = 'block';
+//   sideDrawer.classList.add('open');
+// }
 
 function hamburgerEng(){
   hamburger.classList.toggle("is-active")
 }
-backdrop.addEventListener('click', backdropClickHandler);
-menuToggle.addEventListener('click', menuToggleClickHandler);
+// backdrop.addEventListener('click', backdropClickHandler);
+
 hamburger.addEventListener('click',hamburgerEng)
 
-// $(document).ready(function(){
-//   $(".hamburger").click(function(){
-//     $(this).toggleClass("is-active");
-//   });
-// });
+
