95e3962f3e422d3c1da979a614ec3978302bb8a3
Tamana543 <tamanafarzami33@gmail.com>
2026-02-14 06:40:26 +0430

    file updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,14 @@
-// for navBar
-
 const hamburger = document.querySelector('#hamburger-6');
 const navbar = document.querySelector('.navbar');
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('is-active'); // animation
-  navbar.classList.toggle('open');         // show / hide
+  hamburger.classList.toggle('is-active');
+  navbar.classList.toggle('open');
 });
 
-
+document.querySelectorAll('.links_bar a').forEach(link => {
+  link.addEventListener('click', () => {
+    navbar.classList.remove('open');
+    hamburger.classList.remove('is-active');
+  });
+});
