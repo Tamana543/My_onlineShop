ced71f15a0b80da71dd448bc0882e314b552b90d
Tamana543 <tamanafarzami33@gmail.com>
2026-04-14 09:42:50 +0600

    folder updated

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,27 +1,24 @@
-function deleteProduct(button){
-     const prodId = button.dataset.productId;
-     const csrf = button.dataset.csrf
 
-     
-  fetch('/admin/delete-product/' + prodId, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      'CSRF-Token': csrf
-    }
-  })
-  .then(res => {
-    if (!res.ok) {
-      throw new Error('Delete failed');
-    }
-    return res.json();
-  })
-  .then(() => {
-    button.closest('article').remove();
-  })
-  .catch(err => {
-    console.error(err);
-    alert('Deleting failed');
+function handleAdminDelete(productId, csrfToken) {
+  openConfirm(() => {
+    fetch('/admin/delete-product/' + productId, {
+      method: 'DELETE',
+      headers: {
+        'csrf-token': csrfToken
+      }
+    })
+    .then(res => res.json())
+    .then(() => {
+      location.reload();
+    })
+    .catch(err => console.log(err));
   });
 }
-window.deleteProduct = deleteProduct;
+
+function handleCartDelete(button) {
+  const form = button.closest("form");
+
+  openConfirm(() => {
+    form.submit();
+  });
+}
