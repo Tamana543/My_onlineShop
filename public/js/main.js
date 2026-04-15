ae7d6a993b0c561fc919de44bea4d4bced6bfdf0
Tamana543 <tamanafarzami33@gmail.com>
2026-04-15 06:47:14 +0600

    file udpated

--- public/js/main.js
+++ public/js/main.js
@@ -12,3 +12,36 @@ document.querySelectorAll('.links_bar a').forEach(link => {
     hamburger.classList.remove('is-active');
   });
 });
+
+
+
+function handleAdminDelete(productId, csrfToken) {
+  openConfirm(() => {
+    fetch('/admin/delete-product/' + productId, {
+      method: 'DELETE',
+      headers: {
+        'csrf-token': csrfToken
+      }
+    })
+    .then(res => res.json())
+    .then(() => {
+      location.reload();
+    })
+    .catch(err => console.log(err));
+  });
+}
+
+function handleCartDelete(button) {
+  const form = button.closest("form");
+
+  openConfirm(() => {
+    form.submit();
+  });
+}
+function openConfirm(action) {
+  selectedAction = action;
+  if (!modal) return console.error("Modal not found");
+  modal.classList.remove("hidden");
+}
+window.handleAdminDelete = handleAdminDelete;
+window.handleCartDelete = handleCartDelete;
