b49d727c81e86211d679d78f932ab127ef767cd7
Tamana543 <tamanafarzami33@gmail.com>
2026-05-11 09:45:50 +0600

    rating schma added

--- module/reviews.js
+++ module/reviews.js
@@ -1,3 +1,36 @@
 const mongoose = require('mongoose')
 const Scheme = mongoose.Schema;
 
+const reviewSchema = new Schema({
+     productId: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    },
+
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    username: {
+        type: String,
+        required: true
+    },
+
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+
+    reviewText: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+module.exports = mongoose.model("Review", reviewSchema);
