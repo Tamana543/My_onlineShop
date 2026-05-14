0a3736880d4824d27aa860effa0eded4c402ecf2
Tamana543 <tamanafarzami33@gmail.com>
2026-05-14 20:24:39 +0600

    backend completed for Product sorting (price, newest, etc.),

--- module/product.js
+++ module/product.js
@@ -21,14 +21,14 @@ const SchemaFirstProduct = new Scheme({
      userId : {
           type : Scheme.Types.ObjectId,
           ref : 'User',
-          require : true
+          required : true
      },
      category :{
           type : String, 
           required : true
-     }
+     },
      
-});
+},{timestamps: true });
 SchemaFirstProduct.index({
   title: "text",
   description: "text"
@@ -36,59 +36,4 @@ SchemaFirstProduct.index({
 module.exports = mongoose.model('Product',SchemaFirstProduct)
      
      
-     /**
- },
- productId : {
-      type : Scheme.Types.ObjectId,
-      ref : 'User',
-      required: true
- }
- 
-const { json } = require("body-parser");
-const fs = require("fs");
-const path = require('path')
-const p = path.join(path.dirname(process.mainModule.filename), 'data','products.json')
-const getProductsFromFile =cb => {
-    fs.readFile(p,(err,fileContent)=>{
-          if (err) {
-              return cb([]);
-          }
-          try {
-               
-               cb(JSON.parse(fileContent));
-          } catch (error) {
-               console.log("Error Here :", error);
-               cb([])
-          }
-    })
-}
-module.exports =class Products {
-     constructor(title,imageURL,description , price, ) {
-          this.title = title,
-          this.imageURL = imageURL,
-          this.price = price,
-          this.description = description
-     }
-     save() {
-          this.id = Math.random().toString();
-       getProductsFromFile(product => {
-
-            product.push(this)
-            fs.writeFile(p, JSON.stringify(product),(err)=>{
-                 console.log(err);
-            })
-          })
-     }
-     static findById(id,cb) {
-        getProductsFromFile(products => {
-          const product = products.find(prod => prod.id === id)
-          cb(product)
-        })
-     }
-     static fetchAll(cb) {
-        getProductsFromFile(cb)
-          
-     }
-}
-
- */
+    
